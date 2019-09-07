f689ac4d8e7c6532b55b576b0aeacfc31dd4e8f7
zheyang <yangzhe926@gmail.com>
2019-09-07 17:55:23 -0400

    1[CHG]connect database

--- Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
+++ Vic.SportsStore/Vic.SportsStore.DebugConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.DebugConsole
 {
@@ -10,6 +12,20 @@ namespace Vic.SportsStore.DebugConsole
     {
         static void Main(string[] args)
         {
+            using (var ctx = new EFDbContext())
+            {
+                var product = new Product()
+                {
+                    Name = "banana",
+                    Price = 1.5m,
+                    Description = "this is a banana",
+                    Category = "food"
+                };
+                ctx.Products.Add(product);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
         }
     }
 }

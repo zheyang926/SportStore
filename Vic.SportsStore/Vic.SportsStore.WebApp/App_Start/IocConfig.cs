8ae045f99f0503c1a28508636937dc0ea5a960fa
zheyang <yangzhe926@gmail.com>
2019-09-07 17:05:50 -0400

    1[NEW]add product controller

--- Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
 using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.WebApp
@@ -21,19 +22,22 @@ namespace Vic.SportsStore.WebApp
                 .RegisterControllers(typeof(MvcApplication).Assembly)
                 .PropertiesAutowired();
 
-            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            //Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
 
-            mock
-                .Setup(m => m.Products)
-                .Returns(new List<Product>
-                {
-                    new Product { Name = "Football", Price = 25 },
-                    new Product { Name = "Surf board", Price = 179 },
-                    new Product { Name = "Running shoes", Price = 95 }
-                });
+            //mock
+            //    .Setup(m => m.Products)
+            //    .Returns(new List<Product>
+            //    {
+            //        new Product { Name = "Football", Price = 25 },
+            //        new Product { Name = "Surf board", Price = 179 },
+            //        new Product { Name = "Running shoes", Price = 95 }
+            //    });
 
-            builder.RegisterInstance<IProductsRepository>(mock.Object);
-    
+            //builder.RegisterInstance<IProductsRepository>(mock.Object);
+            builder
+                .RegisterInstance<IProductsRepository>(new InMemoryProductsRepository())
+                .PropertiesAutowired();
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IProductsRepository ProductsRepository { get; set; }
+
+        public ViewResult List()
+        {
+            return View(ProductsRepository.Products);
+        }
+       
+    }
+}

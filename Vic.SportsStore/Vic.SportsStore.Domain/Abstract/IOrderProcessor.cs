5ddd9d9ca8df8cd02a8c3775014c760303a3215d
zheyang <yangzhe926@gmail.com>
2019-09-28 17:18:48 -0400

    1[CHG] add EmailOrderProcessor

---
+++ Vic.SportsStore/Vic.SportsStore.Domain/Abstract/IOrderProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vic.SportsStore.Domain.Entities;
+
+namespace Vic.SportsStore.Domain.Abstract
+{
+    public interface IOrderProcessor
+    {
+        void ProcessOrder(Cart cart, ShippingDetails shippingDetails);
+    }
+}
---
+++ Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vic.SportsStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "sportsstore@example.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";  //search google to find these information
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"d:\sports_store_emails";
+    }
+}

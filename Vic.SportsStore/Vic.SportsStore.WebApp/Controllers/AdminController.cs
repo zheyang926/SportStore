2b5a10183c6d990707744045b092680417c01da8
zheyang <yangzhe926@gmail.com>
2019-10-26 16:06:48 -0400

    1[CHG]add admin page;

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}

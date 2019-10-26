c23349861fee70db3d63e8e3557ed9766b7b508c
zheyang <yangzhe926@gmail.com>
2019-10-26 16:20:46 -0400

    1[CHG]update Product annotation;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/AdminController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -18,6 +19,14 @@ namespace Vic.SportsStore.WebApp.Controllers
         public ViewResult Index()
         {
             return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository
+            .Products
+            .FirstOrDefault(p => p.ProductId == productId); //尝试取一个，如果取不到不抛异常。
+            return View(product);
         }
     }
 }
